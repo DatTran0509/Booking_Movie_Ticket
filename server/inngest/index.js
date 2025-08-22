ab6b53076db5956bdcf0b9b5b3a6cb3ea10c9663
DatTran0509 <dat881070@gmail.com>
2025-08-22 23:15:16 +0700

    update release seats

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -53,7 +53,7 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
     {id: 'release-seats-and-delete-booking'},
     {event: 'app/checkpayment'},
     async ({event, step}) => {
-        const tenMinutesLater = new Date(Date.now() + 1 * 60 * 1000);
+        const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
         await step.sleepUntil('wait-for-10-minutes',tenMinutesLater);
 
         await step.run('check-payment-status', async () => {
